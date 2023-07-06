6ca8e61bae1346e608c5a75bd8ad1ffedbe7108b
mmdev110 <mmdev8787@gmail.com>
2023-07-06 21:14:23 +0000

    fmt

--- tf/prod/SSM.tf
+++ tf/prod/SSM.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "ecr_registry_url" {
   name        = "/backend/ecr_registry_url"
-  value       = split("/",aws_ecr_repository.repository.repository_url)[0]
+  value       = split("/", aws_ecr_repository.repository.repository_url)[0]
   type        = "String"
   description = "URL of ECR registry"
 }
