2103ac8f6c7d5fdd9b35b8439e829285bf8189d5
mmdev110 <mmdev8787@gmail.com>
2023-07-11 15:56:19 +0000

    arrangement

--- tf/prod/Backend/VPC.tf
+++ tf/prod/Backend/VPC.tf
@@ -11,33 +11,13 @@ resource "aws_vpc" "service" {
 resource "aws_internet_gateway" "example" {
   vpc_id = aws_vpc.service.id
 }
-//ルートテーブル自身
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.service.id
-}
-//private用ルートテーブル
-resource "aws_route_table" "private0" {
-  vpc_id = aws_vpc.service.id
-}
-//ルートテーブルの1レコード
-//外部へのアクセスをIGWに飛ばす
-resource "aws_route" "public" {
-  route_table_id         = aws_route_table.public.id
-  gateway_id             = aws_internet_gateway.example.id
-  destination_cidr_block = "0.0.0.0/0"
-}
-
-//パブリックサブネット1
+//ap-northeast-1aについて
+//パブリックサブネット0
 resource "aws_subnet" "public0" {
   vpc_id                  = aws_vpc.service.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-northeast-1a"
-}
-//ルートテーブルとサブネットの関連付け
-resource "aws_route_table_association" "public0" {
-  subnet_id      = aws_subnet.public0.id
-  route_table_id = aws_route_table.public.id
 }
 //プライベートサブネット(web-1a)
 resource "aws_subnet" "web0" {
@@ -53,11 +33,26 @@ resource "aws_subnet" "db0" {
   availability_zone       = "ap-northeast-1a"
   map_public_ip_on_launch = false
 }
-resource "aws_route" "private0" {
-  //外部アクセスをNATゲートウェイに飛ばす
-  route_table_id         = aws_route_table.private0.id
-  nat_gateway_id         = aws_nat_gateway.nat_gateway0.id
+//ルートテーブル: aws_route_table
+//ルートテーブルのレコード: aws_route
+//ルートテーブルとサブネットの紐付け: aws_route_table_association
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.service.id
+}
+resource "aws_route" "public" {
+  //外部へのアクセスをIGWに飛ばす
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.example.id
   destination_cidr_block = "0.0.0.0/0"
+}
+//ルートテーブルとサブネットの紐付け
+resource "aws_route_table_association" "public0" {
+  subnet_id      = aws_subnet.public0.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private0" {
+  vpc_id = aws_vpc.service.id
 }
 resource "aws_route_table_association" "web0" {
   subnet_id      = aws_subnet.web0.id
@@ -66,6 +61,12 @@ resource "aws_route_table_association" "web0" {
 resource "aws_route_table_association" "db0" {
   subnet_id      = aws_subnet.db0.id
   route_table_id = aws_route_table.private0.id
+}
+resource "aws_route" "private0" {
+  //外部アクセスをNATゲートウェイに飛ばす
+  route_table_id         = aws_route_table.private0.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway0.id
+  destination_cidr_block = "0.0.0.0/0"
 }
 //NATゲートウェイ
 resource "aws_nat_gateway" "nat_gateway0" {
@@ -77,4 +78,45 @@ resource "aws_nat_gateway" "nat_gateway0" {
 resource "aws_eip" "nat_gateway0" {
   domain     = "vpc"
   depends_on = [aws_internet_gateway.example]
+}
+
+//ap-northeast-1cについて
+//パブリックサブネット1
+resource "aws_subnet" "public1" {
+  vpc_id                  = aws_vpc.service.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1c"
+}
+//プライベートサブネット(web-1c)
+resource "aws_subnet" "web1" {
+  vpc_id                  = aws_vpc.service.id
+  cidr_block              = "10.0.64.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+}
+//プライベートサブネット(db-1c)
+resource "aws_subnet" "db1" {
+  vpc_id                  = aws_vpc.service.id
+  cidr_block              = "10.0.65.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+}
+resource "aws_route_table" "private1" {
+  vpc_id = aws_vpc.service.id
+}
+resource "aws_route" "private1" {
+  //外部アクセスをNATゲートウェイに飛ばす
+  //NATゲートウェイは1a側のを使いシングル構成とする(コスト都合)
+  route_table_id         = aws_route_table.private1.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway0.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+resource "aws_route_table_association" "web1" {
+  subnet_id      = aws_subnet.web1.id
+  route_table_id = aws_route_table.private1.id
+}
+resource "aws_route_table_association" "db1" {
+  subnet_id      = aws_subnet.db1.id
+  route_table_id = aws_route_table.private1.id
 }
