720537ab2ab0c95136ce1f28da04c167b7afb670
mmdev110 <mmdev8787@gmail.com>
2023-07-14 03:16:39 +0000

    use rds instead of aurora serverless because of cost issue

--- tf/prod/Backend/RDS.tf
+++ tf/prod/Backend/RDS.tf
@@ -1,19 +1,4 @@
 //データベースの設定
-resource "aws_rds_cluster_parameter_group" "db" {
-  name        = "music-tools-db-cluster-parameter-group"
-  family      = "aurora-mysql8.0"
-  description = "DB cluster parameter group for music-tools"
-
-  parameter {
-    name  = "character_set_server"
-    value = "utf8"
-  }
-
-  parameter {
-    name  = "character_set_client"
-    value = "utf8"
-  }
-}
 resource "aws_db_parameter_group" "db" {
   name        = "music-tools-db-parameter-group"
   description = "DB parameter group for music-tools"
@@ -25,6 +10,10 @@ resource "aws_db_parameter_group" "db" {
   parameter {
     name  = "character_set_server"
     value = "utf8mb4"
+  }
+  parameter {
+    name  = "time_zone"
+    value = "Asia/Tokyo"
   }
 }
 
@@ -39,23 +28,20 @@ resource "aws_db_subnet_group" "db" {
   subnet_ids = [data.aws_subnet.db0.id, data.aws_subnet.db1.id]
 }
 
-resource "aws_rds_cluster" "db_cluster" {
-  cluster_identifier = "music-tools-db"
-  engine             = "aurora-mysql"
-  engine_version     = "8.0.mysql_aurora.3.03.1"
-  engine_mode        = "provisioned"
-  //kms_key_id     = aws_kms_key.example.arn
-  serverlessv2_scaling_configuration {
-    max_capacity = 1.0
-    min_capacity = 0.5
-  }
-  master_username = data.aws_ssm_parameter.db_user.value
-  //パスワードは仮置きして後で更新する
-  /*
-  aws rds modify-db-instance --db-instance-identifier "example" --master-user-password "NewMasterPassword!"
-  */
-  master_password = data.aws_ssm_parameter.db_password.value
-  database_name   = data.aws_ssm_parameter.db_database.value
+resource "aws_db_instance" "db" {
+  identifier            = "music-tools-db"
+  engine                = "mysql"
+  engine_version        = "8.0.33"
+  instance_class        = "db.t4g.micro"
+  username              = data.aws_ssm_parameter.db_user.value
+  password              = data.aws_ssm_parameter.db_password.value
+  db_name               = data.aws_ssm_parameter.db_database.value
+  storage_type          = "gp3"
+  storage_encrypted     = true
+  allocated_storage     = 20
+  max_allocated_storage = 100
+  multi_az              = false
+  publicly_accessible   = false
   //multi_az            = false
   //backup_window              = "09:10-09:40"
   //backup_retention_period    = 30
@@ -67,26 +53,18 @@ resource "aws_rds_cluster" "db_cluster" {
   skip_final_snapshot=false
   削除するときは逆にする
   */
-  deletion_protection             = false
-  skip_final_snapshot             = true
-  port                            = 3306
-  apply_immediately               = true
-  vpc_security_group_ids          = [module.mysql_sg.security_group_id]
-  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.db.name
-  //option_group_name               = aws_db_option_group.db.name
-  db_subnet_group_name = aws_db_subnet_group.db.name
+  deletion_protection    = false
+  skip_final_snapshot    = true
+  port                   = 3306
+  apply_immediately      = true
+  vpc_security_group_ids = [module.mysql_sg.security_group_id]
+  option_group_name      = aws_db_option_group.db.name
+  parameter_group_name   = aws_db_parameter_group.db.name
+  db_subnet_group_name   = aws_db_subnet_group.db.name
 
   lifecycle {
-    ignore_changes = [master_password]
+    ignore_changes = [password]
   }
-}
-resource "aws_rds_cluster_instance" "db" {
-  cluster_identifier  = aws_rds_cluster.db_cluster.id
-  instance_class      = "db.serverless"
-  engine              = aws_rds_cluster.db_cluster.engine
-  engine_version      = aws_rds_cluster.db_cluster.engine_version
-  publicly_accessible = false
-
 }
 
 //DBインスタンスのセキュリティグループ
@@ -96,11 +74,4 @@ module "mysql_sg" {
   vpc_id      = data.aws_vpc.service.id
   port        = 3306
   cidr_blocks = [data.aws_vpc.service.cidr_block]
-}
-
-output "endpoint" {
-  value = aws_rds_cluster.db_cluster.endpoint
-}
-output "reader_endpoint" {
-  value = aws_rds_cluster.db_cluster.reader_endpoint
 }
