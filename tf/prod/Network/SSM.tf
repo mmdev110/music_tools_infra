c16cef670d0b65ae977229afa6752b92ce96de07
mmdev110 <mmdev8787@gmail.com>
2023-07-17 09:18:16 +0000

    added parameters for lambda, mediaconvert

--- tf/prod/Network/SSM.tf
+++ tf/prod/Network/SSM.tf
@@ -1,57 +1,32 @@
 //イメージアップロード用
-resource "aws_ssm_parameter" "ecr_registry_url" {
-  name        = "/backend/ecr_registry_url"
-  value       = split("/", aws_ecr_repository.repository.repository_url)[0]
+resource "aws_ssm_parameter" "backend_ecr_registry_url" {
+  name        = "/music_tools/prod/backend/backend_ecr_registry_url"
+  value       = split("/", aws_ecr_repository.backend.repository_url)[0]
   type        = "String"
-  description = "URL of ECR registry"
+  description = "URL of ECR registry for backend"
 }
-resource "aws_ssm_parameter" "ecr_repository_url" {
-  name        = "/backend/ecr_repository_url"
-  value       = aws_ecr_repository.repository.repository_url
+resource "aws_ssm_parameter" "backend_ecr_repository_url" {
+  name        = "/music_tools/prod/backend/backend_ecr_repository_url"
+  value       = aws_ecr_repository.backend.repository_url
   type        = "String"
-  description = "URL of ECR repository"
+  description = "URL of ECR repository for backend"
 }
-//domain names
-//resource "aws_ssm_parameter" "main_domain" {
-//  name        = "/conf/main_domain"
-//  value       = "ys-dev.net"
-//  type        = "String"
-//  description = "main_domain"
-//}
-//resource "aws_ssm_parameter" "backend_domain" {
-//  name        = "/conf/backend_domain"
-//  value       = "backend.music-tools.ys-dev.net"
-//  type        = "String"
-//  description = "backend_domain"
-//}
-//resource "aws_ssm_parameter" "frontend_domain" {
-//  name        = "/conf/frontend_domain"
-//  value       = "music-tools.ys-dev.net"
-//  type        = "String"
-//  description = "frontend_domain"
-//}
-////S3 bucket names
-//resource "aws_ssm_parameter" "bucket_name_usermedia" {
-//  name        = "/conf/bucket_name_usermedia"
-//  value       = "music-tools-media-prod"
-//  type        = "String"
-//  description = "bucket_name_usermedia"
-//}
-//resource "aws_ssm_parameter" "bucket_name_access_log" {
-//  name        = "/conf/bucket_name_access_log"
-//  value       = "music-tools-access-log-prod"
-//  type        = "String"
-//  description = "bucket_name_access_log"
-//}
-//resource "aws_ssm_parameter" "bucket_name_frontend" {
-//  name        = "/conf/bucket_name_frontend"
-//  value       = "music-tools-frontend"
-//  type        = "String"
-//  description = "bucket_name_frontend"
-//}
+//lambdaイメージアップロード用
+resource "aws_ssm_parameter" "lambda_ecr_registry_url" {
+  name        = "/music_tools/prod/backend/lambda_ecr_registry_url"
+  value       = split("/", aws_ecr_repository.lambda.repository_url)[0]
+  type        = "String"
+  description = "URL of ECR registry for lambda"
+}
+resource "aws_ssm_parameter" "lambda_ecr_repository_url" {
+  name        = "/music_tools/prod/backend/lambda_ecr_repository_url"
+  value       = aws_ecr_repository.lambda.repository_url
+  type        = "String"
+  description = "URL of ECR repository for lambda"
+}
 //db parameters
 resource "aws_ssm_parameter" "db_user" {
-  name        = "/db/db_user"
+  name        = "/music_tools/prod/db/db_user"
   value       = "replace me"
   type        = "SecureString"
   description = "db user"
@@ -60,7 +35,7 @@ resource "aws_ssm_parameter" "db_user" {
   }
 }
 resource "aws_ssm_parameter" "db_password" {
-  name        = "/db/db_password"
+  name        = "/music_tools/prod/db/db_password"
   value       = "replace me"
   type        = "SecureString"
   description = "db password"
@@ -69,7 +44,7 @@ resource "aws_ssm_parameter" "db_password" {
   }
 }
 resource "aws_ssm_parameter" "db_database" {
-  name        = "/db/db_database"
+  name        = "/music_tools/prod/db/db_database"
   value       = "replace me"
   type        = "SecureString"
   description = "db database"
@@ -78,10 +53,20 @@ resource "aws_ssm_parameter" "db_database" {
   }
 }
 resource "aws_ssm_parameter" "hmac_secret_key" {
-  name        = "/backend/hmac_secret_key"
+  name        = "/music_tools/prod/backend/hmac_secret_key"
   value       = "replace me"
   type        = "SecureString"
   description = "hmac_secret_key"
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "mediaconvert_endpoint" {
+  name        = "/music_tools/prod/backend/mediaconvert_endpoint"
+  value       = "replace me"
+  type        = "SecureString"
+  description = "mediaconvert_endpoint for prod"
   lifecycle {
     ignore_changes = [value]
   }
