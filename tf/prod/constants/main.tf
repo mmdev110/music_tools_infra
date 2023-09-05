519c7403c74bd4364868133cf7ca83dd130b38c3
mmdev110 <mmdev8787@gmail.com>
2023-09-05 00:53:08 +0000

    organize constants by roles

--- tf/prod/constants/main.tf
+++ tf/prod/constants/main.tf
@@ -1,24 +1,30 @@
 locals {
+  //全般
   service_name = "music_tools"
   env          = "prod"
-
   main_domain     = "ys-dev.net"
-  frontend_domain = "music-tools.ys-dev.net"
-  backend_domain  = "backend.music-tools.ys-dev.net"
-  backend_elb_domain  = "elb-backend.music-tools.ys-dev.net"
-
-  bucket_name_usermedia = "music-tools-media-prod"
-  bucket_name_alb_log   = "music-tools-access-log-prod"
-  bucket_name_frontend  = "music-tools-frontend-prod"
   bucket_name_terraform = "music-tools-infra-prod"
 
+  //backend
+  backend_domain  = "backend.music-tools.ys-dev.net"
+  backend_elb_domain  = "elb-backend.music-tools.ys-dev.net"
+  bucket_name_alb_log   = "music-tools-access-log-prod"
   cloudwatch_backend_log="/music_tools/prod/backend"
-
   ecr_repository_name_backend="music_tools_backend"
-  lambda_function_name="music_tools_lambda_convert_prod"
 
+  //frontend
+  frontend_domain = "music-tools.ys-dev.net"
+  bucket_name_frontend  = "music-tools-frontend-prod"
+
+  //lambda(media_convert)
+  bucket_name_usermedia = "music-tools-media-prod"
+  lambda_function_name="music_tools_lambda_convert_prod"
   mediaconvert_region="ap-northeast-1"
   mediaconvert_job_template_name="music_tools_convert_prod"
+
+  //lambda(backend_manager)
+  backend_manager_domain  = "elb-backend.music-tools.ys-dev.net"
+  ecr_repository_name_backend_manager="music_tools_backend_manager"
 }
 output "service_name" {
   value = local.service_name
